b541e54a48d58ee0ce97ec05e3186b09c1355c5a
Saiganity1 <sicatmichaeldave041@gmail.com>
2026-08-06 12:04:45 +0800

    Fix build error by removing orphaned @RepositoryRestResource annotation

--- backend/src/main/java/com/etayo/backend/repository/UserRepository.java
+++ backend/src/main/java/com/etayo/backend/repository/UserRepository.java
@@ -6,10 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
 @Repository
-@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Boolean existsByEmail(String email);
